add2739c0388af703ea2f2782e0d5e6098eb412b
Mohammed Riyaf Ali M <riyafalim@gmail.com>
2024-11-08 12:23:02 +0530

    COMMIT

--- src/com/tns/day1/CharDemo.java
+++ src/com/tns/day1/CharDemo.java
@@ -3,7 +3,7 @@ package com.tns.day1;
 public class CharDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		 // TODO Auto-generated method stub
 		char ch = 'a';
 		System.out.println(ch);
 		char ch1 = 65;
